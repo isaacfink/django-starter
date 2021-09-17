49445827663e0ada8baed04ea8856d2217fa6696
Isaac Finkelstein <isaacf@alufdevs.com>
2021-09-17 14:05:53 -0400

    not working yet

---
+++ sample/sample/settings/development.py
@@ -0,0 +1,11 @@
+from .base import *
+
+DEBUG = True
+
+INSTALLED_APPS += [
+    
+]
+
+MIDDLEWARE += ['debug_toolbar.middleware.DebugToolbarMiddleware', ]
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
