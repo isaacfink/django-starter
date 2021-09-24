9df7bbce90294e2f885051731e12d7f5304868e0
Isaac Finkelstein <isaacf@alufdevs.com>
2021-09-24 08:26:28 -0400

    frontend started

---
+++ test-tracking-frontend/src/App.svelte
@@ -0,0 +1,33 @@
+<script>
+	import { Router, Link, Route } from "svelte-navigator";
+  import Dashboard from './pages/Dashboard.svelte'
+  import Employees from './pages/Employees.svelte'
+  import Login from './pages/Login.svelte'
+  import Nav from './components/Nav.svelte'
+</script>
+
+<Router>
+
+	<div>
+		<Route path="/">
+			<Dashboard />
+		</Route>
+
+    	<Route path="/employees">
+			<Employees />
+		</Route>
+
+		<Route path="/login">
+			<Login />
+		</Route>
+	</div>
+</Router>
+
+<style>
+  :global(h1){
+    outline:none;
+  }
+  :global(h2){
+	outline:none;
+  }
+</style>
